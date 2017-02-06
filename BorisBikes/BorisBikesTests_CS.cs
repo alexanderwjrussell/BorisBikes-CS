0b3a009736c98e43c55e5dbceea264bd714f1c56
alexanderwjrussell <awjrussell@live.co.uk>
2017-02-06 16:20:18 +0000

    Setting up the initial project

---
+++ BorisBikes/BorisBikesTests_CS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BorisBikes
+{
+    [TestFixture]
+    public class BorisBikesTests_CS
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
